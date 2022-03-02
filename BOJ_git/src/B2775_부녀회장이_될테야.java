5725a7ef338472988125a84cedec75de511314d6
Park Juyeon <18cwceark@gmail.com>
2022-03-02 09:47:41 +0900

    Update 2775

--- BOJ_git/src/B2775_부녀회장이_될테야.java
+++ BOJ_git/src/B2775_부녀회장이_될테야.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,22 +6,22 @@ import java.io.InputStreamReader;
 public class B2775_부녀회장이_될테야 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine()); // 테스트케이스
 		for (int tc = 1; tc <= T; tc++) {
-			int k = Integer.parseInt(br.readLine());
-			int n = Integer.parseInt(br.readLine());
+			int k = Integer.parseInt(br.readLine()); // 층
+			int n = Integer.parseInt(br.readLine()); // 호
 			System.out.println(calkon(k, n));
 		}
 		br.close();
 	}
 
 	public static int calkon(int k, int n) {
-		if (k == 0) {
+		if (k == 0) { // 0층에는 n명
 			return n;
-		} else if (n == 1) {
+		} else if (n == 1) { // 1호에는 1명
 			return 1;
 		} else {
-			return calkon(k - 1, n) + calkon(k, n - 1);
+			return calkon(k - 1, n) + calkon(k, n - 1); // 재귀호출
 		}
 	}
 }
