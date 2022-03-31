b8449bcb947bdf6f9ba81f94a99093069336b2ab
Park Juyeon <18cwceark@gmail.com>
2022-03-31 10:34:57 +0900

    Upload 1436

---
+++ BOJ_git/src/B1436_영화감독_숌_1.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 666부터 1 증가(브루트포스)
+// contains() 사용
+public class B1436_영화감독_숌_1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 편 수
+		int ans = 666;
+		int cnt = 1;
+		while (cnt < N) {
+			if (Integer.toString(++ans).contains("666")) cnt++;
+		}
+		System.out.println(ans);
+		br.close();
+	}
+}
