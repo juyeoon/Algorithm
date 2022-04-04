d9a7e0eb047eab9151f5f83a7e95616c39af6e5e
Park Juyeon <18cwceark@gmail.com>
2022-04-04 17:21:51 +0900

    Upload 5337

---
+++ BOJ_git/src/B5337_웰컴.java
@@ -0,0 +1,7 @@
+public class B5337_웰컴 {
+	public static void main(String[] args) {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
