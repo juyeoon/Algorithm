1766584bf5f4776d5bdb5229f847ef297943b8b4
Park Juyeon <18cwceark@gmail.com>
2022-04-06 13:11:52 +0900

    Upload 5339

---
+++ BOJ_git/src/B5339_콜센터.java
@@ -0,0 +1,15 @@
+// '\'출력 => "\\" 사용
+public class B5339_콜센터 {
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
