636de1aa91eb7663f0b337b61306ac572c0b54e6
Park Juyeon <18cwceark@gmail.com>
2022-04-23 21:18:55 +0900

    Upload 5338

---
+++ BOJ_git/src/B5338_마이크로소프트_로고.java
@@ -0,0 +1,10 @@
+// 단순 출력 => ' -> \'
+public class B5338_마이크로소프트_로고 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("\'-..-\'|   ||   |");
+		System.out.println("\'-..-\'|_.-;;-._|");
+		System.out.println("\'-..-\'|   ||   |");
+		System.out.println("\'-..-\'|_.-\'\'-._|");
+	}
+}
