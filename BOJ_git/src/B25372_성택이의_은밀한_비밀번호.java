59d6fee603e704d49a3ef2f5c825ba55c6f55fa6
Park Juyeon <18cwceark@gmail.com>
2022-07-18 13:22:50 +0900

    Upload 25372

---
+++ BOJ_git/src/B25372_성택이의_은밀한_비밀번호.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 입력 문자열 길이 체크
+public class B25372_성택이의_은밀한_비밀번호 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine()); // 비밀번호 개수
+		for (int tc = 0; tc < N; tc++) {
+			String s = br.readLine(); // 비밀번호 문자열
+			if (s.length() >= 6 && s.length() <= 9) { // 6자 이상 9자 이하
+				sb.append("yes\n");
+			} else {
+				sb.append("no\n");
+			}
+		}
+		System.out.println(sb); // 출력
+		br.close();
+	}
+}
