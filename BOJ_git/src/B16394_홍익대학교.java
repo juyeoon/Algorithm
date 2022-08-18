87c35f8ba939b4b80745bc951fa4be5fedab2806
박주연 <18cwceark@gmail.com>
2022-08-18 21:47:44 +0900

    Upload 16394

---
+++ BOJ_git/src/B16394_홍익대학교.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+// 개교 연도인 1946을 입력받은 연도에서 빼기
+public class B16394_홍익대학교 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() - 1946);
+	}
+}
