eb26f5828c72468f38698d45fcc3801ab04043fd
박주연 <18cwceark@gmail.com>
2022-08-18 21:44:56 +0900

    Upload 24262

---
+++ BOJ_git/src/B24262_알고리즘_수업_알고리즘의_수행_시간_1.java
@@ -0,0 +1,7 @@
+// 주어진 알고리즘은 재귀가 없으므로 시행횟수는 1번, 수행시간은 상수 시간이 소요된다.
+// 단순 출력
+public class B24262_알고리즘_수업_알고리즘의_수행_시간_1 {
+	public static void main(String[] args) {
+		System.out.println("1\n0");
+	}
+}
