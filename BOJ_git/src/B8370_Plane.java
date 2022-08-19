217401bd235deddb787e53d21f71e83353835311
박주연 <18cwceark@gmail.com>
2022-08-19 14:11:13 +0900

    Upload 8370

---
+++ BOJ_git/src/B8370_Plane.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+// 비즈니스 클래스 행*열 + 이코노미 클래스 행*열 출력
+public class B8370_Plane {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() * sc.nextInt() + sc.nextInt() * sc.nextInt()); // 출력
+	}
+}
