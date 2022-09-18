719611fb67ae9df259c0efea2c3c207e681b2335
Park Juyeon <18cwceark@gmail.com>
2022-09-18 11:16:58 +0900

    Update 10953

--- BOJ_git/src/B10953_A_더하기_B_6번.java
+++ BOJ_git/src/B10953_A_더하기_B_6번.java
@@ -2,23 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 10953 - A+B - 6
-// https://www.acmicpc.net/problem/10953
+// split(",")로 A, B 구분하기
 public class B10953_A_더하기_B_6번 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-//		StringTokenizer st = null;
-//		StringTokenizer st = new StringTokenizer(br.readLine());
-//		String s[] = br.readLine().split(" ");
-//		double R = Double.parseDouble(s[0]);
-//		double C = Double.parseDouble(s[1]);
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine()); // 테스트케이스
 		for (int tc = 0; tc < T; tc++) {
 			String s[] = br.readLine().split(",");
-			sb.append(Integer.parseInt(s[0])+Integer.parseInt(s[1])).append("\n");
+			sb.append(Integer.parseInt(s[0]) + Integer.parseInt(s[1])).append("\n"); // A+B
 		}
-		System.out.println(sb);
+		System.out.println(sb); // 출력
 		br.close();
 	}
 }
