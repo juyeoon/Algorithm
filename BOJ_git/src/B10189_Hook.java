6c2c780451dcc3f5ee1eb2feb38475acecf303c3
박주연 <18cwceark@gmail.com>
2023-04-25 16:32:11 +0900

    Upload 10189

---
+++ BOJ_git/src/B10189_Hook.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 단순 출력
+public class B10189_Hook {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#  # #### #### #  #");
+		br.close();
+	}
+}
