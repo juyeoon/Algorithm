75d8cf4332c08380e40ead83ac49a944420434a3
박주연 <18cwceark@gmail.com>
2024-02-08 14:03:43 +0900

    Upload 13311

---
+++ BOJ_git/src/B13311_행운의_편지.java
@@ -0,0 +1,7 @@
+// 2부터 1000까지 정수를 n으로 나눴을 때, 나머지가 모두 같음을 만족하는 n 구하기
+// -1로 나눴을 때 나머지가 -1이므로 -1 출력
+public class B13311_행운의_편지 {
+	public static void main(String[] args) {
+		System.out.println(-1);
+	}
+}
