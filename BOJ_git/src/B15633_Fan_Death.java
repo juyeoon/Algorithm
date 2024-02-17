64743ec058edacb0f4b7329c240a39a8903ed4c4
박주연 <18cwceark@gmail.com>
2024-02-17 18:53:48 +0900

    Rewrite 15633

--- BOJ_git/src/B15633_Fan_Death.java
+++ BOJ_git/src/B15633_Fan_Death.java
@@ -4,28 +4,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-// 15633 - Fan Death
-// https://www.acmicpc.net/problem/15633
+// 1부터 n 제곱근까지 반복하면서 약수 쌍(약수 작은 수 * 약수 큰 수 = n인 경우)을 구함
 public class B15633_Fan_Death {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//		StringBuilder sb = new StringBuilder();
-//		StringTokenizer st = null;
-//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int n = Integer.parseInt(br.readLine());
-		int end = (int) Math.sqrt(n);
-		long sum = 0;
-		for (int i = 1; i <= end; i++) {
-			if (n % i == 0) {
+		int n = Integer.parseInt(br.readLine()); // 자연수 n
+		int end = (int) Math.sqrt(n); // 약수 구할 반복문의 범위(n 제곱근)
+		long sum = 0; // 약수의 합
+		for (int i = 1; i <= end; i++) { // 약수 구하기
+			if (n % i == 0) { // 약수 중 작은 값
 				sum += i;
-				if (n / i != i) {
+				if (n / i != i) { // 약수 중 큰 값
 					sum += n / i;
 				}
 			}
 		}
-		bw.write(String.valueOf(sum * 5 - 24));
+		bw.write(String.valueOf(sum * 5 - 24)); // 출력
 		bw.flush();
 		bw.close();
 		br.close();
