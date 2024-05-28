2a0b2e5c2d38e27864511f97ca3d628b188bf129
박주연 <18cwceark@gmail.com>
2024-05-28 14:15:29 +0900

    Upload 23804

---
+++ BOJ_git/src/B23804_골뱅이_찍기_ㄷ.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// N*5개 N줄
+// N개 N*3줄
+// N*5개 N줄 출력
+public class B23804_골뱅이_찍기_ㄷ {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) { // N*5개 N줄
+			for (int j = N * 5; j > 0; j--) {
+				sb.append('@');
+			}
+			sb.append("\n");
+		}
+		for (int i = N * 3; i > 0; i--) { // N개 N*3줄
+			for (int j = 0; j < N; j++) {
+				sb.append('@');
+			}
+			sb.append("\n");
+		}
+		for (int i = 0; i < N; i++) { // N*5개 N줄
+			for (int j = N * 5; j > 0; j--) {
+				sb.append('@');
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb); // 출력
+		br.close();
+	}
+}
