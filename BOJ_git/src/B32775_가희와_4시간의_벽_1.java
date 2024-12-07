c69ee4d8faac15ddbf34b5fa5eb0a3707ce6ccc6
박주연 <18cwceark@gmail.com>
2024-12-07 20:10:30 +0900

    Modify 32775 comment

--- BOJ_git/src/B32775_가희와_4시간의_벽_1.java
+++ BOJ_git/src/B32775_가희와_4시간의_벽_1.java
@@ -7,8 +7,8 @@ import java.io.InputStreamReader;
 public class B32775_가희와_4시간의_벽_1 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int Sab = Integer.parseInt(br.readLine()); // 항공기를 이용하여 이동한 시간
-		int Fab = Integer.parseInt(br.readLine()); // 철도를 이용하여 이동한 시간
+		int Sab = Integer.parseInt(br.readLine()); // 철도를 이용하여 이동한 시간
+		int Fab = Integer.parseInt(br.readLine()); // 항공기를 이용하여 이동한 시간
 		System.out.println(Sab <= Fab ? "high speed rail" : "flight"); // 출력
 		br.close();
 	}
