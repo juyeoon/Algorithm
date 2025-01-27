de33087e13ad23c8c0b3a75127b2929b98fd00a4
Park Juyeon <18cwceark@gmail.com>
2025-01-27 18:25:20 +0900

    Fix 6030

--- BOJ_git/src/B6030_Scavenger_Hunt.java
+++ BOJ_git/src/B6030_Scavenger_Hunt.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/6030
 // P와 Q의 약수를 찾고, 각 수의 조합을 모두 출력하는 문제
 // 각각의 수에 대해서 반복문으로 약수를 찾고 ArrayList에 저장하여 조합을 출력 
-public class Main {
+public class B6030_Scavenger_Hunt {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
