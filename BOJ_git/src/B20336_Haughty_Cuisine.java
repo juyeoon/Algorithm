eabf8ba82f035cdc76a9b57cc05287469e50dc31
박주연 <18cwceark@gmail.com>
2025-02-11 15:44:51 +0900

    Update 20336

--- BOJ_git/src/B20336_Haughty_Cuisine.java
+++ BOJ_git/src/B20336_Haughty_Cuisine.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// https://www.acmicpc.net/problem/20336
 // 입력받은 세트 메뉴 중 하나를 골라 그 세트의 메뉴 수와 메뉴(순서 상관 X)를 출력
 // => 첫번째로 입력받은 메뉴를 " "을 "\n"로 변환하여 출력
 public class B20336_Haughty_Cuisine {
