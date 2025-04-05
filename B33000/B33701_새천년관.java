fbf90f14a1e10d26d2290dcc82262c7da9192a52
박주연 <18cwceark@gmail.com>
2025-04-05 20:35:49 +0900

    Fix 33701 - Change Class Name

--- B33000/B33701_새천년관.java
+++ B33000/B33701_새천년관.java
@@ -1,7 +1,7 @@
 // https://www.acmicpc.net/problem/33701
 // 조건에 맞는 문자열 중 아무거나 하나 출력하는 문제
 // gwan으로 끝나고 맨 뒤 글자와 맨 앞의 글자가 같으면서 k와 u가 들어가는 문자열을 출력
-public class Main {
+public class B33701_새천년관 {
 	public static void main(String[] args) {
 		System.out.print("nkugwan"); // 출력
 	}
