26bf2086ba162d8effde838b9dedc3280c9dd18e
박주연 <18cwceark@gmail.com>
2025-06-25 13:29:19 +0900

    Upload 2441

--- B02000/B2441_별_찍기_4.java
+++ B02000/B2441_별_찍기_4.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// https://www.acmicpc.net/problem/2441
 // 이중 for문을 이용하여 별 찍기
 // (행 번호)만큼 공백을 찍은 후, (N - 행 번호)만큼 별 찍고 다음 행으로
 public class B2441_별_찍기_4 {
