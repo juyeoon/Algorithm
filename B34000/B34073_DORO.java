7cda232ce2aaa5fb7aa32fb28558b85557de3eb4
박주연 <18cwceark@gmail.com>
2025-07-28 23:30:24 +0900

    Upload 34073

---
+++ B34000/B34073_DORO.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/34073
+// 단순 구현 문제
+public class B34073_DORO {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			sb.append(st.nextToken()).append("DORO ");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+}
