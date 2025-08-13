59c9c85c09614e1f826b1cd99c1b5a7349e23df3
Park Juyeon <18cwceark@gmail.com>
2025-08-13 22:18:03 +0900

    Upload 14209

---
+++ B14000/B14209_Bridž.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/14209
+// 조건 분기로 모든 패의 점수를 합산하여 출력
+public class B14209_Bridž {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 카드 패를 받은 횟수
+    long total = 0; // 전체 명예 점수 합계
+		for (int i = 0; i < N; i++) {
+  		String s = br.readLine(); // 카드 패
+      for (int j = 0; j < s.length(); j++) { // 점수 합산
+        char c = s.charAt(j);
+        switch (c) {
+          case 'A': 
+            total += 4; 
+            break;
+          case 'K': 
+            total += 3; 
+            break;
+          case 'Q': 
+            total += 2; 
+            break;
+          case 'J': 
+            total += 1; 
+            break;
+        }
+      }
+		}
+		System.out.println(total); // 출력
+		br.close();
+	}
+}
