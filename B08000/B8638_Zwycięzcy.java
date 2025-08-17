ce677925702f4e003b2271d5b8f6b669813647f7
Park Juyeon <18cwceark@gmail.com>
2025-08-17 21:41:44 +0900

    Upload 8638

---
+++ B08000/B8638_Zwycięzcy.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/8638
+// 전체 점수 중 최댓값을 구한 후 최댓값을 가지는 선수 번호를 출력
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine()); // 선수 수
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int[] score = new int[N]; // 각 선수의 점수
+    int max = 0; // 점수의 최댓값
+    for (int i = 0; i < N; i++) {
+        score[i] = Integer.parseInt(st.nextToken());
+        if (score[i] > max) { // 최댓값 갱신
+            max = score[i];
+        }
+    }
+    for (int i = 0; i < N; i++) { // 최댓값을 가지는 선수 찾기
+         if (score[i] == max) {
+            sb.append((char) ('A' + i)); // 선수 번호 출력 저장
+        }
+    }
+		System.out.println(sb); // 출력
+		br.close();
+	}
+}
