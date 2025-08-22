fc5393687bea206ad77639f7ee7923b13c23dfbe
Park Juyeon <18cwceark@gmail.com>
2025-08-22 20:04:58 +0900

    Upload 11520

---
+++ B11000/B11520_And_Then_There_Was_5.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Q번째 숫자는 항상 D, R번째 숫자는 항상 5이므로 테스트케이스마다 "D 5"를 출력
+// https://www.acmicpc.net/problem/11520
+public class B11520_And_Then_There_Was_5 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine()); // 테스트케이스
+        for (int t = 0; t < N; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            st.nextToken();
+            int D = Integer.parseInt(st.nextToken());
+            sb.append(D).append(" 5\n"); // 출력 저장
+        }
+        System.out.print(sb); // 출력        
+		br.close();
+	}
+}
