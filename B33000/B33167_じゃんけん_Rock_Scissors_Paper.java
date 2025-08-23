67c4f865bb1d6ddf77bdf11e82381c8c31725c9a
박주연 <18cwceark@gmail.com>
2025-08-23 19:11:42 +0900

    Chore 33167

--- B33000/B33167_じゃんけん_Rock_Scissors_Paper.java
+++ B33000/B33167_じゃんけん_Rock_Scissors_Paper.java
@@ -9,17 +9,17 @@ public class B33167_じゃんけん_Rock_Scissors_Paper {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-        String S = br.readLine(); // Aoi의 손동작
-        String T = br.readLine(); // Bitaro의 손동작
-        int aWin = 0; // Aoi 승 수
-        int bWin = 0; // Bitaro 승 수
-        for (int i = 0; i < N; i++) {
+		String S = br.readLine(); // Aoi의 손동작
+		String T = br.readLine(); // Bitaro의 손동작
+		int aWin = 0; // Aoi 승 수
+		int bWin = 0; // Bitaro 승 수
+		for (int i = 0; i < N; i++) {
 			char a = S.charAt(i);
 			char b = T.charAt(i);
 			if (a == 'S' && b == 'P') { // Aoi 승: (S, P)
 				aWin++;
 			}
-            // Bitaro 승: (R, P) 또는 (S, R)
+			// Bitaro 승: (R, P) 또는 (S, R)
 			else if ((a == 'R' && b == 'P') || (a == 'S' && b == 'R')) {
 				bWin++;
 			}
