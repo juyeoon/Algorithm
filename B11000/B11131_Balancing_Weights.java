0ca3387285cd20ee1bf4c755bc25b51dd5a70c7c
박주연 <18cwceark@gmail.com>
2025-09-23 15:42:59 +0900

    style: apply prettier formatting

--- B11000/B11131_Balancing_Weights.java
+++ B11000/B11131_Balancing_Weights.java
@@ -6,27 +6,27 @@ import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/11131
 // 입력된 수들의 합을 구한 후 음수, 양수, 0인지 확인하여 출력하는 문제
 public class B11131_Balancing_Weights {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스
-        for (int tc = 0; tc < T; tc++) {
-            int N = Integer.parseInt(br.readLine()); // 가중치 개수
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int sum = 0; // 가중치 합
-            for (int i = 0; i < N; i++) { // sum 구하기
-                sum += Integer.parseInt(st.nextToken());
-            }
-            if(sum < 0) { // 왼쪽으로 기욺
-                sb.append("Left");
-            } else if(sum > 0) { // 오른쪽으로 기욺
-                sb.append("Right");
-            } else { // 평형
-                sb.append("Equilibrium");
-            }
-            sb.append("\n");   
-        }
-        System.out.println(sb); // 출력
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine()); // 테스트 케이스
+		for (int tc = 0; tc < T; tc++) {
+			int N = Integer.parseInt(br.readLine()); // 가중치 개수
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int sum = 0; // 가중치 합
+			for (int i = 0; i < N; i++) { // sum 구하기
+				sum += Integer.parseInt(st.nextToken());
+			}
+			if (sum < 0) { // 왼쪽으로 기욺
+				sb.append("Left");
+			} else if (sum > 0) { // 오른쪽으로 기욺
+				sb.append("Right");
+			} else { // 평형
+				sb.append("Equilibrium");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb); // 출력
+		br.close();
+	}
 }
--- B11000/B11368_A_Serious_Reading_Problem.java
+++ B11000/B11368_A_Serious_Reading_Problem.java
@@ -9,20 +9,20 @@ import java.math.BigInteger;
 // 총 경우의 수 = 가능한 문자의 수 ^ 한 책의 총 문자 수
 // 수가 매우 커질 수 있으므로 BigInteger 사용
 public class B11368_A_Serious_Reading_Problem {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String s = "";
-        while (!(s = br.readLine()).equals("0 0 0 0")) { // EOI
-            StringTokenizer st = new StringTokenizer(s);
-            int C = Integer.parseInt(st.nextToken()); // 가능한 문자의 수
-            int W = Integer.parseInt(st.nextToken()); // 한 줄의 문자의 수
-            int L = Integer.parseInt(st.nextToken()); // 한 페이지의 줄 개수
-            int P = Integer.parseInt(st.nextToken()); // 한 책의 페이지 수
-            int leng = W * L * P; // 한 책의 총 문자 수
-            sb.append(BigInteger.valueOf(C).pow(leng)).append("\n"); // 총 경우의 수
-        }
-        System.out.println(sb); // 출력
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String s = "";
+		while (!(s = br.readLine()).equals("0 0 0 0")) { // EOI
+			StringTokenizer st = new StringTokenizer(s);
+			int C = Integer.parseInt(st.nextToken()); // 가능한 문자의 수
+			int W = Integer.parseInt(st.nextToken()); // 한 줄의 문자의 수
+			int L = Integer.parseInt(st.nextToken()); // 한 페이지의 줄 개수
+			int P = Integer.parseInt(st.nextToken()); // 한 책의 페이지 수
+			int leng = W * L * P; // 한 책의 총 문자 수
+			sb.append(BigInteger.valueOf(C).pow(leng)).append("\n"); // 총 경우의 수
+		}
+		System.out.println(sb); // 출력
+		br.close();
+	}
 }
