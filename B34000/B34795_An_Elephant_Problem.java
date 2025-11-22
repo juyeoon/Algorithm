ae9d4d82c82869b71a3ead1fb08f7b8ab9b59bd3
박주연 <18cwceark@gmail.com>
2025-11-22 19:20:18 +0900

    Upload 34795

---
+++ B34000/B34795_An_Elephant_Problem.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/34795
+public class B34795_An_Elephant_Problem {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int p = (d + m - 1) / m;
+		System.out.println(p);
+		br.close();
+	}
+}
