a558621bc1cbe61b7cb8443cd7a06741f95026ce
박주연 <18cwceark@gmail.com>
2026-04-16 23:12:19 +0900

    Upload 32888

---
+++ B32000/B32888_Consolidating_Windows.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+// 32888 - Consolidating Windows
+// https://www.acmicpc.net/problem/32888
+public class B32888_Consolidating_Windows {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		double result = Math.sqrt((double)a * a + (double)b * b);
+		System.out.printf("%.10f\n", result);
+		br.close();
+	}
+}
